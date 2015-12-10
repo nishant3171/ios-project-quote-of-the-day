a38e8a7f2c687e126130c3cc9726772e07472626
Nishant Punia <nishantpunia3171@gmail.com>
2015-12-10 10:00:38 +0530

    First project files

--- Life Experience/ViewController.swift
+++ Life Experience/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DailyQuote: UIImageView!
+    @IBOutlet weak var uncoolBg: UIImageView!
+    @IBOutlet weak var clickButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func HIdingButton(sender: AnyObject) {
+        DailyQuote.hidden = false
+        uncoolBg.hidden = false
+        clickButton.hidden = true
+    }
 
 }
 
